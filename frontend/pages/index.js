643d85075d4990a9e4085772c748e8cad19baa25
Dirk Bradshaw <dirk.bradshaw@gmail.com>
2025-12-05 14:46:03 -0500

    Fix map container height

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,9 +4,16 @@ const MapView = dynamic(() => import("../components/MapView"), { ssr: false });
 
 export default function Home() {
   return (
-    <div className="w-full h-screen">
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        margin: 0,
+        padding: 0,
+        overflow: "hidden",
+      }}
+    >
       <MapView />
     </div>
   );
 }
-
