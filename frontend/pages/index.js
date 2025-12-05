05ccc13931b84b4e44b7098610bc569525bed023
Dirk Bradshaw <dirk.bradshaw@gmail.com>
2025-12-05 14:50:02 -0500

    Make map full-screen

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -6,11 +6,8 @@ export default function Home() {
   return (
     <div
       style={{
-        width: "100vw",
-        height: "100vh",
-        margin: 0,
-        padding: 0,
-        overflow: "hidden",
+        width: "100%",
+        height: "100%",
       }}
     >
       <MapView />
